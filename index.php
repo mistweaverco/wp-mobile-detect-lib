66e689b9b5b53f5720d484b7dd20f17ecd1fd2ae
Marco Kellershoff <hello@gorilla.moe>
2018-09-21 10:34:32 +0200

    Update php files according to php-cs-fixer/linter

--- index.php
+++ index.php
@@ -1,2 +1,1 @@
 <?php echo 'Here be dragons!';
-
--- mobile-detect-lib.php
+++ mobile-detect-lib.php
@@ -5,19 +5,22 @@
  * Author: Marco Kellershoff
  * Author URI: https://about.walialu.com/
  * Version: 1.0.0
- * Description: Make the awesome `Mobile_Detect.php` globally lib available 
+ * Description: Make the awesome `Mobile_Detect.php` globally lib available
  */
 
-class WP_Mobile_Detect_Lib {
-        function __construct() {
-        }
+class WP_Mobile_Detect_Lib
+{
+    public function __construct()
+    {
+    }
 }
 
-if(!class_exists("Mobile_Detect"))
-        require_once(__DIR__ . "/lib/Mobile_Detect.php");
-
-
-function getMobileDetectLib() {
-        return new Mobile_Detect;
+if (!class_exists("Mobile_Detect")) {
+    require_once(__DIR__ . "/lib/Mobile_Detect.php");
 }
 
+
+function getMobileDetectLib()
+{
+    return new Mobile_Detect;
+}
